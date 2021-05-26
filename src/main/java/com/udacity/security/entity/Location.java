783e236b02cd31e8808eeed0c9fa20ad7289bd40
Janak Soni <janaksoni91@gmail.com>
2021-05-26 00:08:02 -0500

    Reformat

--- src/main/java/com/udacity/security/entity/Location.java
+++ src/main/java/com/udacity/security/entity/Location.java
@@ -1,6 +1,9 @@
 package com.udacity.security.entity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Location {
@@ -22,7 +25,8 @@ public class Location {
         this.address = address;
     }
 
-    public Location() {}
+    public Location() {
+    }
 
     public Long getId() {
         return id;
